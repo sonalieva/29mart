c00faf93acc7a7c8c1bbf2f51e306f2fca01a92b
sonalieva <708v8lg@code.edu.az>
2022-03-29 10:03:36 +0400

    created 29mart

---
+++ 29mart/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _29mart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("UserName ni daxil edin");
+            string username = Console.ReadLine();
+
+            Console.WriteLine("Passwordu daxil edin");
+            string password = Console.ReadLine();
+            User user = new User(username)
+            {
+                Password = password
+            };
+            user.GetInfo();
+
+
+        }
+    }
+}
